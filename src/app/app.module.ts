fab72a362a0de2364ed31f601ba40bfe6b14a563
Beka Sturua <beqasturua3@gmail.com>
2023-03-12 15:43:29 +0400

    Assignment  Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from "@angular/forms"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warnign-alert.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
   ],
   imports: [
     BrowserModule,
